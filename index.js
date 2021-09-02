913b529e2d9bdc25d78b8652943829ba28b79f3d
Anton Brisilin <a.brisilin@innopolis.university>
2021-09-03 00:25:31 +0300

    Added pretty name for modules

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+const { response } = require('express')
 const express = require('express')
+
+const get_module_name = require('./utils/module_name')
 
 const app = express()
 
 const startServer = ({ port }) => {
+    const moduleName = get_module_name()
+    const appPath = `/${moduleName}`
+
+    app.get(appPath, (request, response) => {
+        response.send(`
+            <body>
+                <h1>Hello ${moduleName}</h1>
+            </body>
+        `)
+    })
+
     app.listen(port, () => {
-        console.log(`server started at http://localhost:${port}`)
+        console.log(`server started at http://localhost:${port}${appPath}`)
     })
 }
 
-module.exports = { "startServer": startServer }
+module.exports = startServer
