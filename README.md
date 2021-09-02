2dc93c51df194362158ef0595e35023163484b2f
Anton Brisilin <a.brisilin@innopolis.university>
2021-09-02 23:36:46 +0300

    add startServer function

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tolst-dev-server
+
 This is the development server for the developers
---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+
+const startServer = ({port}) => {
+    app.listen(port, () => {
+        console.log(`server started at http://localhost:${port}`)
+    })
+}
