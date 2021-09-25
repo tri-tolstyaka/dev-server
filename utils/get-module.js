117de4feceb718f2bac6eead54a3dc63dec37383
Anton Brisilin <a.brisilin@innopolis.university>
2021-09-25 12:50:23 +0300

    fixup-final

--- utils/get-module.js
+++ utils/get-module.js
@@ -1,13 +1,8 @@
 const path = require("path");
 
 // if there are global node_modules at the project root directory
-const getModulePath = (libName, libFilePath) => {
-	let subpath = __dirname.split("/")
-	console.log("subpath", subpath)
-	subpath = subpath.slice(0, subpath.length - 4)
-	const dirname = subpath.join("/")
-	path.resolve(dirname, "node_modules", libName, libFilePath);
-}
+const getModulePath = (libName, libFilePath) =>
+	path.resolve("node_modules", libName, libFilePath);
 
 module.exports = (req, res) => {
 	const libName = req.params[0];
