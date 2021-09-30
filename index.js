afb1648c724a5b46b703d20b52004347acfdede7
Shuvani <gnomik2311@mail.ru>
2021-09-30 20:16:59 +0300

    add extended webpack config

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ const startServer = ({ port }) => {
 		module: {
 			rules: [
 				{
+					test: /\.(js)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react']
+						}
+					},
+				},
+				{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
+				{
 					test: /\.tsx?$/,
 					loader: "ts-loader",
 				},
